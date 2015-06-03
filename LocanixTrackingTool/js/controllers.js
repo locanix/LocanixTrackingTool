367b8b0aa51fc6b03e414d4edb9b99af00ce9d90
locanix <jayanti.prajapat@locanix.com>
2015-06-03 11:28:59 +0530

    First checked in

---
+++ LocanixTrackingTool/js/controllers.js
@@ -0,0 +1,49 @@
+'use strict';
+
+angular.module('myApp.controllers', [])
+    .controller('MainCtrl', ['$scope', '$rootScope', '$window', '$location', function ($scope, $rootScope, $window, $location) {
+        $scope.slide = '';
+        $rootScope.back = function () {
+            $scope.slide = 'slide-right';
+            $window.history.back();
+        }
+        $rootScope.go = function (path) {
+            $scope.slide = 'slide-left';
+            $location.url(path);
+        }
+    }])
+    .controller('EmployeeListCtrl', ['$scope', function ($scope) {
+        //$scope.employees = Employee.query();
+        //$scope.employees = localStorage.getItem('todos');
+        $scope.trackCode = '';
+
+        $scope.trackCodeArray = [];
+
+        $scope.addTrackCode = function () {
+            $scope.trackCodeArray.push({ code: "'" + $scope.trackCode + "'" });
+
+            $scope.trackCode = ''; //clear the input after adding
+            localStorage.setItem('trackCodeArray', JSON.stringify($scope.trackCodeArray));
+
+            $scope.trackCodeArray = JSON.parse(localStorage.getItem('trackCodeArray'));
+
+        }
+        $scope.trackCodeArray = (localStorage.getItem('trackCodeArray') !== null) ? JSON.parse(localStorage.getItem('trackCodeArray')) : [];
+
+        $scope.deleteTrackCode = function (index) {
+            $scope.trackCodeArray.splice(index, 1);
+            localStorage.setItem('trackCodeArray', JSON.stringify($scope.trackCodeArray));
+        }
+
+
+    }])
+    .controller('EmployeeDetailCtrl', ['$scope', '$routeParams', '$sce', function ($scope, $routeParams, $sce) {
+
+
+        $scope.trackCodeCode = $routeParams.code;
+
+        $scope.trackURL = $sce.trustAsResourceUrl('http://server2.locanix.net/TrackingTool/?t=' + $routeParams.code);
+    }])
+    .controller('ReportListCtrl', ['$scope', '$routeParams', 'Report', function ($scope, $routeParams, Report) {
+        $scope.employees = Report.query({ employeeId: $routeParams.employeeId });
+    }])
